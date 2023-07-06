4320a9c6990798e63c7ab318d1c28e3efd1c7068
Svitlana Trotsenko <trots.svit@gmail.com>
2023-07-06 03:08:17 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+
+    const fetchData = async() => {
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            setData(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return ( <
+            div >
+            <
+            h1 > Posts from API: < /h1> <
+            button onClick = { fetchData } > Posts < /button> {
+            data ? ( <
+                ul > {
+                    data.map((post) => ( <
+                        li key = { post.id } >
+                        <
+                        h2 > { post.title } < /h2> <
+                        p > { post.body } < /p> < /
+                        li >
+                    ))
+                } <
+                /ul>
+            ) : ( <
+                p > Click the button to fetch data < /p>
+            )
+        } <
+        /div>
+);
 }
 
 export default App;
